8600c6f4cfaf7086408009940637b8df7e4c463c
yanbo <yangyongbao@126.com>
2017-04-28 16:40:19 +0800

    enable external clock

--- user/config.c
+++ user/config.c
@@ -14,8 +14,81 @@ void NVIC_Configuration(void)
 
 void RCC_Configuration(void)
 {
-    /* GPIOE clock enable */
-    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOE, ENABLE);
+    ErrorStatus HSEStartUpStatus;
+
+    /* RCC system reset(for debug purpose) */
+    RCC_DeInit();
+
+    /* Enable HSE */
+    RCC_HSEConfig(RCC_HSE_ON);
+
+    /* Wait till HSE is ready */
+    HSEStartUpStatus = RCC_WaitForHSEStartUp();
+
+    if (HSEStartUpStatus == SUCCESS) {
+        /* Enable Prefetch Buffer */
+        // FLASH_PrefetchBufferCmd(FLASH_PrefetchBuffer_Enable);
+
+        /* Flash 2 wait state */
+        // FLASH_SetLatency(FLASH_Latency_2);
+
+        /* HCLK = SYSCLK */
+        RCC_HCLKConfig(RCC_SYSCLK_Div1);
+
+        /* PCLK2 = HCLK */
+        RCC_PCLK2Config(RCC_HCLK_Div1);
+
+        /* PCLK1 = HCLK/2 */
+        RCC_PCLK1Config(RCC_HCLK_Div2);
+
+        /* ADCCLK = PCLK2/4 */
+        RCC_ADCCLKConfig(RCC_PCLK2_Div4);
+
+        /**************************** Configure PLLs ******************************/
+        /* PLL2 configuration: PLL2CLK = (HSE / 5) * 8 = 40 MHz */
+        RCC_PREDIV2Config(RCC_PREDIV2_Div5);
+        RCC_PLL2Config(RCC_PLL2Mul_8);
+
+        /* Enable PLL2 */
+        RCC_PLL2Cmd(ENABLE);
+
+        /* Wait till PLL2 is ready */
+        while (RCC_GetFlagStatus(RCC_FLAG_PLL2RDY) == RESET) {}
+
+        /* PLL configuration: PLLCLK = (PLL2 / 5) * 9 = 72 MHz */
+        RCC_PREDIV1Config(RCC_PREDIV1_Source_PLL2, RCC_PREDIV1_Div5);
+        RCC_PLLConfig(RCC_PLLSource_PREDIV1, RCC_PLLMul_9);
+
+        /* Enable PLL */
+        RCC_PLLCmd(ENABLE);
+
+        /* Wait till PLL is ready */
+        while (RCC_GetFlagStatus(RCC_FLAG_PLLRDY) == RESET) {}
+
+        /* Select PLL as system clock source */
+        RCC_SYSCLKConfig(RCC_SYSCLKSource_PLLCLK);
+
+        /* Wait till PLL is used as system clock source */
+        while (RCC_GetSYSCLKSource() != 0x08) {}
+    }
+
+    /*******************************Enable peripheral clocks***************************/
+
+    /* Enable DMA1 clock */
+    RCC_AHBPeriphClockCmd(RCC_AHBPeriph_DMA1, ENABLE);
+
+    /* DMA2 clock enable */
+    RCC_AHBPeriphClockCmd(RCC_AHBPeriph_DMA2, ENABLE);
+
+    /* Enable ADC1 and GPIOC clock */
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_ADC1 | RCC_APB2Periph_GPIOC, ENABLE);
+
+    /* GPIOA Periph clock enable */
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA | RCC_APB2Periph_AFIO, ENABLE);
+    /* DAC Periph clock enable */
+    RCC_APB1PeriphClockCmd(RCC_APB1Periph_DAC, ENABLE);
+    /* TIM2 Periph clock enable */
+    RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM2, ENABLE);
 }
 
 void GPIO_Configuration(void)
